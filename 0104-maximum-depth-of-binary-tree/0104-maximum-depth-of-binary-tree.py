10a553fd3e85862a6d0ab67c7d937976fb6de65b
Daeul Lee <daeul.lee12@gmail.com>
2023-05-20 16:59:35 +0900

    Time: 20 ms (92.42%), Space: 16.1 MB (56.1%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -20,23 +20,42 @@ class Solution(object):
         # without recursion, BFS & DFS available
         # Solution 2: Breadth-first search using Queue
 
+        # if not root:
+        #     return 0
+
+        # q = deque([root])
+        # level = 0
+        
+        # while q:
+        #     for i in range(len(q)):
+        #         node = q.popleft()
+        #         if node.left:
+        #             q.append(node.left)
+        #         if node.right:
+        #             q.append(node.right)            
+        
+        #     level += 1
+
+        # return level
+
+        # Solution 3: Depth-First Search
         if not root:
             return 0
-                
-        q = deque([root])
-        level = 0
-        
-        while q:
-            for i in range(len(q)):
-                node = q.popleft()
-                if node.left:
-                    q.append(node.left)
-                if node.right:
-                    q.append(node.right)            
-        
-            level += 1
 
-        return level
+        s = [[root, 1]]
+        res = 1
+
+        while s:
+            node, dep = s.pop()
+            res = max(res, dep)
+            if node.right:
+                s.append([node.right, dep+1])
+            if node.left:
+                s.append([node.left, dep+1])
+            
+        return res
+
+
             
 
 
