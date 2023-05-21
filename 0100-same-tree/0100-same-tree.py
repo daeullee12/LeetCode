cc9ef93aba515af61c0a3200757b24be7abf1431
Daeul Lee <daeul.lee12@gmail.com>
2023-05-21 13:14:33 +0900

    Time: 15 ms (77.37%), Space: 13.5 MB (60.94%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -12,14 +12,40 @@ class Solution(object):
         :rtype: bool
         """
 
-        # TC O(p+q)
-        if not p and not q:
-            return True
+        # Solution 1: recursive call, TC O(p+q)
+        # if not p and not q:
+        #     return True
     
-        elif not p or not q or p.val != q.val:
-            return False
+        # if not p or not q or p.val != q.val:
+        #     return False
+        # return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
 
-        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        # Solution 2: BFS 
+        
+        if not p and not q: return True
+        if not p or not q: return False
+
+        queue = deque([[p, q]])
+     
+        while queue:
+            
+            for i in range(len(queue)):
+                node1, node2 = queue.popleft()
+                if not node1 and not node2:
+                    continue
+                elif not node1 or not node2 or node1.val != node2.val:
+                    return False
+                
+                queue.append([node1.left, node2.left])
+                queue.append([node1.right, node2.right])
+
+                    
+        if not queue:
+            return True
+       
+        return False
+
+
 
 
                 
