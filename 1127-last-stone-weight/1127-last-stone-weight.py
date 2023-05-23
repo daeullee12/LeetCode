896f40464e418fa3ead35fad1411aac6f4ea6b07
Daeul Lee <daeul.lee12@gmail.com>
2023-05-23 22:01:07 +0900

    Time: 24 ms (24.04%), Space: 13.5 MB (46.9%) - LeetHub

--- 1127-last-stone-weight/1127-last-stone-weight.py
+++ 1127-last-stone-weight/1127-last-stone-weight.py
@@ -1,3 +1,5 @@
+from heapq import *
+
 class Solution(object):
     def lastStoneWeight(self, stones):
         """
@@ -5,14 +7,28 @@ class Solution(object):
         :rtype: int
         """
 
+        # solution 1: max() -> TC O(n)
+        # while len(stones) > 1:
+        #     first = stones.pop(stones.index(max(stones)))
+        #     second = stones.pop(stones.index(max(stones)))
+        #     print(first, second)
+        #     if first != second:
+        #         stones.append(abs(first-second))
+
+        # if len(stones) == 1:
+        #     return stones.pop()
+        # return 0
+
+        # Heapify() -> O(1)
+        stones = [n * (-1) for n in stones]
+
         while len(stones) > 1:
-            first = stones.pop(stones.index(max(stones)))
-            second = stones.pop(stones.index(max(stones)))
-            print(first, second)
+            heapify(stones)
+            first = heappop(stones)
+            second = heappop(stones)
             if first != second:
-                stones.append(abs(first-second))
+                heappush(stones, first - second)
 
         if len(stones) == 1:
-            return stones.pop()
+            return abs(heappop(stones))
         return 0
-            
