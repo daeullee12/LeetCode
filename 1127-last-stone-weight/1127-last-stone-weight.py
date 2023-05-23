41d414182b6cf6a9a442138a8d850ee4e12e4534
Daeul Lee <daeul.lee12@gmail.com>
2023-05-23 22:17:49 +0900

    Time: 23 ms (33.32%), Space: 13.2 MB (99.3%) - LeetHub

--- 1127-last-stone-weight/1127-last-stone-weight.py
+++ 1127-last-stone-weight/1127-last-stone-weight.py
@@ -19,26 +19,26 @@ class Solution(object):
         # return 0
 
         # solution 2: Heap -> TC O(nlogn)
-        # stones = [-n for n in stones] 
-        # heapq.heapify(stones)
+        stones = [-n for n in stones] 
+        heapq.heapify(stones) # O(nlogn)
 
-        # while len(stones) > 1:
-        #     first = heapq.heappop(stones)
-        #     second = heapq.heappop(stones)
-        #     if first != second:
-        #         heapq.heappush(stones, first - second)
-
-        # stones.append(0)
-        # return abs(stones[0])
-
-        #solution 3: sort() -> TC O(nlogn) * n
- 
         while len(stones) > 1:
-            stones.sort()
-            first = stones.pop()
-            second = stones.pop()
+            first = heapq.heappop(stones) # O(logn)
+            second = heapq.heappop(stones) # O(logn)
             if first != second:
-                stones.append(abs(first - second))
+                heapq.heappush(stones, first - second)
 
         stones.append(0)
         return abs(stones[0])
+
+        # solution 3: sort() -> TC O(nlogn) * n
+ 
+        # while len(stones) > 1:
+        #     stones.sort()
+        #     first = stones.pop()
+        #     second = stones.pop()
+        #     if first != second:
+        #         stones.append(abs(first - second))
+
+        # stones.append(0)
+        # return abs(stones[0])
