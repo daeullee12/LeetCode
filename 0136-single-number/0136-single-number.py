9543796396632e5e0440169cb01f1d7892679464
Daeul Lee <daeul.lee12@gmail.com>
2023-05-27 08:06:07 +0900

    Time: 112 ms (50.87%), Space: 15.4 MB (86.36%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -4,14 +4,21 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        q = set()
+        # Solustion 1: Hashset, TC O(n), SC O(n)
+        # q = set()
 
-        for n in nums:
-            if n not in q:
-                q.add(n)
-            else:
-                q.remove(n)
+        # for n in nums:
+        #     if n not in q:
+        #         q.add(n)
+        #     else:
+        #         q.remove(n)
         
-        return list(q)[0]
+        # return list(q)[0]
                 
-                
+        # Solution 2: bit manipulation TC O(n), O(1)
+
+        res = 0
+        for n in nums:
+            res = n ^ res
+
+        return res        
