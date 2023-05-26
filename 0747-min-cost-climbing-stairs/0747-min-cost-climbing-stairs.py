68962105cc2669d409ddaf4991c9ae3bb22cf53f
Daeul Lee <daeul.lee12@gmail.com>
2023-05-26 11:41:21 +0900

    Time: 39 ms (65.36%), Space: 13.4 MB (94.25%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.py
@@ -4,14 +4,18 @@ class Solution(object):
         :type cost: List[int]
         :rtype: int
         """
-
-        def helper(i, minCost):
-            if i == 0 or i == 1:
-                return 0
-            if i in minCost:
-                return minCost[i]
-            minCost[i] = min(helper(i-1,minCost) + cost[i-1], helper(i-2, minCost) + cost[i-2])
-            return minCost[i]
+        # solution1: Memoization: TC O(n), SC O(n)
+        # def helper(i, minCost):
+        #     if i == 0 or i == 1:
+        #         return 0
+        #     if i in minCost:
+        #         return minCost[i]
+        #     minCost[i] = min(helper(i-1,minCost) + cost[i-1], helper(i-2, minCost) + cost[i-2])
+        #     return minCost[i]
         
-        return helper(len(cost),{})
+        # return helper(len(cost),{})
 
+        one, two = 0 , 0
+        for i in range(2,len(cost)+1):
+            one, two = two, min(one + cost[i-2], two + cost[i-1])
+        return two
