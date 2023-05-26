eb6e52f86935db0ca77d1f3f0dce3404d762789a
Daeul Lee <daeul.lee12@gmail.com>
2023-05-26 09:38:46 +0900

    Time: 21 ms (29.74%), Space: 13.5 MB (25.74%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -5,24 +5,26 @@ class Solution(object):
         :rtype: int
         """
         # Brute Fore TC O(2^n)
-        # Memoization TC O(n) SC O(n)
-        return self.helper(n, {})
 
-    def helper(self, n, data):
-        if n == 0:
-            return 0
-        elif n == 1:
-            return 1
-        elif n == 2:
-            return 2
+
+        # Memoization TC O(n) SC O(n)
+    #     return self.helper(n, {})
+
+    # def helper(self, n, data):
+    #     if n == 0:
+    #         return 0
+    #     elif n == 1:
+    #         return 1
+    #     elif n == 2:
+    #         return 2
             
-        if n in data:
-            return data[n]
-        data[n] = self.helper(n-1, data) + self.helper(n-2, data)
-        return data[n]
+    #     if n in data:
+    #         return data[n]
+    #     data[n] = self.helper(n-1, data) + self.helper(n-2, data)
+    #     return data[n]
 
         # # Dynamic Programming TC O(n) SC O(1)
-        # a, b = 1, 1
-        # for i in range(n):
-        #     a, b = b, a + b
-        # return a
+        a, b = 1, 1
+        for i in range(n):
+            a, b = b, a + b
+        return a
