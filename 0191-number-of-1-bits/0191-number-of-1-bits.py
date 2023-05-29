ceb7f6131b00052c2d89ea0684cf305336a88c35
Daeul Lee <daeul.lee12@gmail.com>
2023-05-29 15:07:57 +0900

    Time: 22 ms (35.69%), Space: 13 MB (99.94%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.py
+++ 0191-number-of-1-bits/0191-number-of-1-bits.py
@@ -4,11 +4,19 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        # Solution 1: mod
+        # Solution 1: mod, TC O(1), SC O(1)
+        # res = 0
+        # while n:
+        #     res += n % 2
+        #     n >>= 1
+        
+        # return res
+
+        # Solution 2: & TC O(1), SC O(1)
         res = 0
         while n:
-            res += n % 2
-            n >>= 1
-        
+            n &= (n-1)
+            res += 1
         return res
+
         
