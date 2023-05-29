3a8c0e98b637a02527c0d878885e2fb0f84c3dd9
Daeul Lee <daeul.lee12@gmail.com>
2023-05-29 18:21:41 +0900

    Time: 66 ms (65.48%), Space: 17.4 MB (38.37%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -6,12 +6,22 @@ class Solution(object):
         """
 
         # Solution 1: TC O(nlogn)
-        res = [0]
-        for i in range(1,n+1):
-            count = 0
-            while i:
-                count += i % 2
-                i = i // 2 
-            res.append(count)
+        # res = [0]
+        # for i in range(1,n+1):
+        #     count = 0
+        #     while i:
+        #         count += i % 2
+        #         i = i // 2 
+        #     res.append(count)
 
-        return res
+        # return res
+
+        ans = [0] * (n+1)
+        offset = 1
+        for i in range(1, n + 1):
+            if offset * 2 == i:
+                offset = i
+            ans[i] = 1 + ans[i - offset]
+            
+        return ans
+
