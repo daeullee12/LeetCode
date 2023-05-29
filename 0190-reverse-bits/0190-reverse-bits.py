ae595b05d3295d7f33fa6f8e244b4aa7694c4d73
Daeul Lee <daeul.lee12@gmail.com>
2023-05-30 08:53:09 +0900

    Time: 16 ms (79.18%), Space: 13.4 MB (20.57%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -0,0 +1,14 @@
+class Solution:
+    # @param n, an integer
+    # @return an integer
+    def reverseBits(self, n):
+        o = str()
+
+        while n:
+            o += str(n % 2)
+            n >>= 1
+        
+        while len(o) < 32:
+            o += str(0)
+
+        return(int(o, 2))
