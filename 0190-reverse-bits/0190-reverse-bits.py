14534860f0fd97e32d8f3de81f265dbd01ac32e3
Daeul Lee <daeul.lee12@gmail.com>
2023-05-30 09:33:39 +0900

    Time: 24 ms (27.51%), Space: 13.2 MB (71.62%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -2,13 +2,21 @@ class Solution:
     # @param n, an integer
     # @return an integer
     def reverseBits(self, n):
-        o = str()
+        # o = str()
 
-        while n:
-            o += str(n % 2)
-            n >>= 1
+        # while n:
+        #     o += str(n % 2)
+        #     n >>= 1
         
-        while len(o) < 32:
-            o += str(0)
+        # while len(o) < 32:
+        #     o += str(0)
 
-        return(int(o, 2))
+        # return(int(o, 2))
+
+        res = 0
+
+        for i in range(32):
+            bit = (n >> i) & 1
+            res |= bit << (31 - i)
+        
+        return res
