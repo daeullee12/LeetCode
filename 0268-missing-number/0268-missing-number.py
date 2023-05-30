2b91897222f2c0bd5fe585fe7f3d8316c07802d3
Daeul Lee <daeul.lee12@gmail.com>
2023-05-31 07:58:48 +0900

    Time: 111 ms (49.21%), Space: 14.7 MB (55.54%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -4,14 +4,25 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        # Solution 1: Hasing
-        hmap = {i: None for i in range(len(nums))}
+        # Solution 1: Hasing TC O(n), SC O(n)
+        # hmap = {i: None for i in range(len(nums))}
 
-        for n in nums:
-            hmap.pop(n, None)
+        # for n in nums:
+        #     hmap.pop(n, None)
 
-        if hmap:
-            return list(hmap.keys())[0]    
-        return len(nums)
+        # if hmap:
+        #     return list(hmap.keys())[0]    
+        # return len(nums)
         
+        # Solution 2: XOR TC O(n), SC O(n)
 
+        # missing = len(nums)
+        # for i, n in enumerate(nums):
+        #     missing ^= i ^ n
+        # return missing
+
+        # Solution 3: Sum
+        res = len(nums)
+        for i in range(len(nums)):
+            res += (i - nums[i])
+        return res
