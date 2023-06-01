88e1b6be3011acded7584e4edd72de8b1252c372
Daeul Lee <daeul.lee12@gmail.com>
2023-06-01 09:47:02 +0900

    Time: 20 ms (58.3%), Space: 13.2 MB (99.02%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -4,20 +4,33 @@ class Solution(object):
         :type digits: List[int]
         :rtype: List[int]
         """
-        digits = digits[::-1]
-        carry, i = 1, 0
-        while carry:
-            if i < len(digits):
-                if digits[i] == 9:
-                    digits[i] = 0
-                else: 
-                    digits[i] += 1
-                    carry = 0
+        # digits = digits[::-1]
+        # carry, i = 1, 0
+        # while carry:
+        #     if i < len(digits):
+        #         if digits[i] == 9:
+        #             digits[i] = 0
+        #         else: 
+        #             digits[i] += 1
+        #             carry = 0
             
-            else:
-                digits.append(1)
-                carry = 0
+        #     else:
+        #         digits.append(1)
+        #         carry = 0
 
-            i += 1
+        #     i += 1
         
-        return digits[::-1]
+        # return digits[::-1]
+
+        carry = 1
+        for i in range(len(digits)-1,-1,-1):         
+            digits[i] += carry
+            if digits[i] == 10:
+                digits[i] = 0 
+            else: carry = 0
+
+            if i == 0 and carry:
+                digits.insert(0, carry)
+                carry = 0
+            
+        return digits
