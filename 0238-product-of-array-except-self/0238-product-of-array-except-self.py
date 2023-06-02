2aad79e1bd784f5b905e9de0e97d9450ef1994cd
Daeul Lee <daeul.lee12@gmail.com>
2023-06-02 11:07:40 +0900

    Time: 183 ms (91.42%), Space: 20.7 MB (81.19%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -4,13 +4,25 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        output = [1] * (len(nums))
-        prefix, postfix = 1, 1
+        # output = [1] * (len(nums))
+        # prefix, postfix = 1, 1
 
-        for i in range(1,len(nums)):
-            if i > 0:
-                prefix *= nums[i-1]
-                postfix *= nums[len(nums)-i]
-            output[i] *= prefix
-            output[len(nums)-1-i] *= postfix
-        return output
+        # for i in range(1,len(nums)):
+        #     if i > 0:
+        #         prefix *= nums[i-1]
+        #         postfix *= nums[len(nums)-i]
+        #     output[i] *= prefix
+        #     output[len(nums)-1-i] *= postfix
+        # return output
+
+        res = [1] * (len(nums))
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+
+        postfix = 1
+        for i in range(len(nums)-1,-1,-1):
+            res[i] *= postfix
+            postfix *= nums[i]
+        return res
