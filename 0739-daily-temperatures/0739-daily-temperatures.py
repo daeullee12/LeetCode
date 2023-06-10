449c8d79e76f9d54e2faf6fc59185ae3c4147930
Daeul Lee <daeul.lee12@gmail.com>
2023-06-10 18:11:30 +0900

    Time: 1286 ms (31.69%), Space: 30.2 MB (20.4%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -8,12 +8,12 @@ class Solution(object):
         res = [0] * len(temperatures)
         stack = [] # [index, temp]
 
-        for i, t1 in enumerate(temperatures):
-            while stack and t1 > stack[-1][1]:
-                j, t2 = stack.pop()
-                res[j] = i - j
+        for i, t in enumerate(temperatures):
+            while stack and t > stack[-1][1]:
+                stackInd, stackT = stack.pop()
+                res[stackInd] = i - stackInd
             
-            stack.append([i, t1])
+            stack.append([i, t])
              
         return res
 
