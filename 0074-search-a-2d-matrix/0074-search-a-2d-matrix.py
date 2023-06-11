311c88af8de3400072b94a63cc0228e4cac32595
Daeul Lee <daeul.lee12@gmail.com>
2023-06-11 13:54:46 +0900

    Time: 66 ms (15.91%), Space: 16.8 MB (74.9%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -8,21 +8,15 @@ class Solution:
             rm = r1 + ((r2 - r1) // 2)
             cm = c1 + ((c2 - c1) // 2)
 
-            if matrix[rm][cm] > target:
-                if matrix[rm][0] > target:
-                    r2 = rm - 1
-                else:
-                    c2 = cm - 1
-            elif matrix[rm][cm] < target:
-                if matrix[rm][c2] < target:
-                    r1 = rm + 1
-                else:
-                    c1 = cm + 1
+            if matrix[rm][0] < target < matrix[rm][cm]:
+                c2 = cm - 1
+            elif target < matrix[rm][0]:
+                r2 = rm - 1
+            elif matrix[rm][cm] < target < matrix[rm][c2]:
+                c1 = cm + 1
+            elif target > matrix[rm][c2]:
+                r1 = rm + 1
                 
             else:
                 return True
         return False
-
-# matrix[r1][c1] matrix[r1][cm] matrix[r1][c2]
-# matrix[rm][0] matrix[rm][cm] matrix[rm][c2]
-# matrix[r2][0] matrix[r2][cm] matrix[r2][c2]
