2722c1e3e6cb1a2df2d975aa3bf6c0487d77aff3
Daeul Lee <daeul.lee12@gmail.com>
2023-06-11 12:16:06 +0900

    Time: 1015 ms (34.34%), Space: 35.1 MB (89.54%) - LeetHub

--- 0883-car-fleet/0883-car-fleet.py
+++ 0883-car-fleet/0883-car-fleet.py
@@ -1,11 +1,30 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        pair = [[p, s] for p, s in zip(position, speed)]
-        stack = []
+        
+        # solution 1: stack, TC O(n) SC O(n)
+        # pair = [[p, s] for p, s in zip(position, speed)]
+        # stack = []
 
-        for p, s in sorted(pair)[::-1]:
-            stack.append((target - p) / s)
-            if len(stack) >= 2 and stack[-1] <= stack[-2]:
-                stack.pop()
+        # for p, s in sorted(pair)[::-1]:
+        #     stack.append((target - p) / s)
+        #     if len(stack) >= 2 and stack[-1] <= stack[-2]:
+        #         stack.pop()
                 
-        return len(stack)
+        # return len(stack)
+
+        # Solution 2: 
+        pair = [[p, s] for p, s in zip(position, speed)]
+        pair.sort(reverse=True)
+
+        slowest = -1
+        fleet = 0
+
+        if len(pair) == 1:
+            return 1
+        
+        for p, s in pair:
+            if (target - p) / s > slowest:
+                slowest = (target - p) / s
+                fleet += 1
+        
+        return fleet
