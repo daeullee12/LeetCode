d0b3a06cc7955c8ea50fb75215c5796c8df2abcb
Daeul Lee <daeul.lee12@gmail.com>
2023-06-24 17:15:53 +0900

    Time: 89 ms (78.29%), Space: 20.9 MB (48.45%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -7,14 +7,17 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        
+        """
+        a binary search tree (BST), also called an ordered or sorted binary tree, is a rooted binary tree data structure with the key of each internal node being greater than all the keys in the respective node's left subtree and less than the ones in its right subtree.
+        """
 
         cur = root
 
         while cur:
-            if p.val > cur.val and q.val > cur.val:
-                cur = cur.right
-            elif p.val < cur.val and q.val < cur.val:
+            if cur.val > p.val and cur.val > q.val:
                 cur = cur.left
+            elif cur.val < p.val and cur.val < q.val:
+                cur = cur.right
             else: return cur
-
         
