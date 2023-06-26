031c48fb1ad5efb38c8629ff7e2af11e931cc5cc
Daeul Lee <daeul.lee12@gmail.com>
2023-06-26 14:31:29 +0900

    Time: 876 ms (26.7%), Space: 78.5 MB (29.12%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,4 +1,5 @@
 class Node:
+    
     def __init__(self, key, val):
         self.key, self.val = key, val
         self.prev = self.next = None
@@ -7,19 +8,19 @@ class LRUCache:
 
     def __init__(self, capacity: int):
         self.capa = capacity
-        self.cache = {} # map key to Node
-        # left: LRU, right: most recent 
+        self.cache = {} # map key to node
+        # left = LRU, right = most recent
         self.left, self.right = Node(0, 0), Node(0, 0)
         self.left.next, self.right.prev = self.right, self.left
-    
+
     def remove(self, node):
         prev, nxt = node.prev, node.next
         prev.next, nxt.prev = nxt, prev
-    
+
     def insert(self, node):
         prev, nxt = self.right.prev, self.right
         prev.next = nxt.prev = node
-        node.next, node.prev = nxt, prev
+        node.prev, node.next = prev, nxt
 
     def get(self, key: int) -> int:
         if key in self.cache:
@@ -29,26 +30,17 @@ class LRUCache:
         return -1
 
     def put(self, key: int, value: int) -> None:
-
-
         if key in self.cache:
             self.remove(self.cache[key])
         self.cache[key] = Node(key, value)
         self.insert(self.cache[key])
-
+    
         if len(self.cache) > self.capa:
-            # remove LRU
             lru = self.left.next
             self.remove(lru)
             del self.cache[lru.key]
 
 
-        
-            
-
-    
-    
-        
 
 
 # Your LRUCache object will be instantiated and called as such:
