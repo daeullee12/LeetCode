28f919a2cce9db61cd508babe74e96d66269c882
Daeul Lee <daeul.lee12@gmail.com>
2023-06-26 15:06:21 +0900

    Time: 616 ms (61.61%), Space: 31 MB (75.73%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -3,20 +3,21 @@ class Solution:
         # solution 1: linked list cycle
         # solution 2: Floyd's
         
+        # Floyd's algorithm
+
         slow, fast = 0, 0
 
         while True:
             slow = nums[slow]
             fast = nums[nums[fast]]
-            
             if slow == fast:
                 break
+        
         slow2 = 0
         while True:
             slow = nums[slow]
             slow2 = nums[slow2]
 
             if slow == slow2:
-                break
-        return slow
+                return slow
                 
