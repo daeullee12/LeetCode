bdf125c25f465232d84d0161be86f5beaa521e58
Daeul Lee <daeul.lee12@gmail.com>
2023-07-07 12:23:52 +0900

    Time: 39 ms (92.73%), Space: 16.5 MB (17.39%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,26 +1,26 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-
+        # TC O(n*4^n), n = len(digits)
         res = []
+        digitToChar = { "2" : "abc",
+                        "3" : "def",
+                        "4" : "ghi",
+                        "5" : "jkl",
+                        "6" : "mno",
+                        "7" : "pqrs",
+                        "8" : "tuv",
+                        "9" : "wxyz"}
 
-        word = []
-
-        if len(digits) == 0:
-            return []
-            
-        def backtrack(i):
-            if i == len(digits):
-                res.append(''.join(word))
+        def backtrack(i, curChar):
+            if len(curChar) == len(digits):
+                res.append(curChar)
                 return
-            
-            letters = self.mapping(digits[i])
-            for j in range(len(letters)):
-                word.append(letters[j])
-                backtrack(i + 1)
-                word.pop()
         
-        backtrack(0)
-
+            for c in digitToChar[digits[i]]:
+                backtrack(i + 1, curChar + c)
+        
+        if digits:
+            backtrack(0, "")
         return res
 
         
