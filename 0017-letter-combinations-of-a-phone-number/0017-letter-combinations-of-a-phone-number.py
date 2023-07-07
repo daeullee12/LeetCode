f9bf18241a931416beba4879289fb19d1681946b
Daeul Lee <daeul.lee12@gmail.com>
2023-07-07 12:24:04 +0900

    Time: 44 ms (74.06%), Space: 16.4 MB (17.39%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -22,15 +22,3 @@ class Solution:
         if digits:
             backtrack(0, "")
         return res
-
-        
-    def mapping(self, digit):
-        dic = {2:["a","b","c"],
-            3:["d","e","f"],
-            4:["g","h","i"],
-            5:["j","k","l"],
-            6:["m","n","o"],
-            7:["p","q","r","s"],
-            8:["t","u","v"],
-            9:["w","x","y","z"]}
-        return dic[int(digit)]
