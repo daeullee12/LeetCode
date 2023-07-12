9bd518bfdef8f2e27302250bb29dca377ac58369
Daeul Lee <daeul.lee12@gmail.com>
2023-07-12 09:51:51 +0900

    Time: 22 ms (32.86%), Space: 13.3 MB (74.45%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -4,11 +4,7 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-
-        if len(nums) == 1:
-            return nums[0]
-
-        return max(self.helper(nums[:-1]), self.helper(nums[1:]))
+        return max(nums[0], self.helper(nums[:-1]), self.helper(nums[1:]))
 
     def helper(self, nums):
         one, two = 0, 0
