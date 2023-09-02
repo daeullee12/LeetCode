b5abb740342a5f23e779633a16e25a5e43ab2740
Daeul Lee <daeul.lee12@gmail.com>
2023-09-01 22:24:18 -0500

    Time: 42 ms (64.23%), Space: 16.3 MB (74.25%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -6,6 +6,7 @@ class Solution:
         if n == 0:
             return 1
         
+        # return self.myPow(x * x, n // 2) * self.myPow(x, n % 2)
         if n % 2 != 0:
             return x * self.myPow(x, n - 1)
 
