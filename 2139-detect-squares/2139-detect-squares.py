fe8880675b375c23cbb763d7e35fd45513157e99
Daeul Lee <daeul.lee12@gmail.com>
2023-09-10 21:28:21 +0900

    Time: 502 ms (37.02%), Space: 18.8 MB (10.91%) - LeetHub

--- 2139-detect-squares/2139-detect-squares.py
+++ 2139-detect-squares/2139-detect-squares.py
@@ -9,14 +9,13 @@ class DetectSquares:
         self.ptsCount[tuple(point)] += 1
 
     def count(self, point: List[int]) -> int:
+        x, y = point
         res = 0
-        px, py = point
-        for x, y in self.points:
-            if abs(px - x) != abs(py - y) or x == px or y == py:
+        for px, py in self.points:
+            if abs(x - px) != abs(y - py) or x == px or y == py:
                 continue
             res += self.ptsCount[(x, py)] * self.ptsCount[(px, y)]
         return res
-
 
 
 # Your DetectSquares object will be instantiated and called as such:
