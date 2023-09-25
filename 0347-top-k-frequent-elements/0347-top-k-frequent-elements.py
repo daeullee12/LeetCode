99704d115370ee8749942a6b8ae9f8b9ce94acf3
Daeul Lee <daeul.lee12@gmail.com>
2023-09-25 23:16:36 +0900

    Time: 104 ms (35.17%), Space: 22 MB (19.9%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,28 +1,20 @@
-class Solution(object):
-    def topKFrequent(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: List[int]
-        """
-        #solution 1: bucket sort -> O(klogn), O(k)
-
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        
         count = {}
         freq = [[] for i in range(len(nums) + 1)]
 
         for n in nums:
-            count[n] = 1 + count.get(n, 0)
-
+            count[n] = count.get(n, 0) + 1
+        
         for n, c in count.items():
             freq[c].append(n)
 
         res = []
-        
         for i in range(len(freq) - 1, 0, -1):
             for n in freq[i]:
                 res.append(n)
-                if len(res) == k:
-                    return res
+            if len(res) == k:
+                return res
 
-                
-
+        
