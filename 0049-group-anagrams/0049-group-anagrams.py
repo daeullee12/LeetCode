a8695409ab0d402a97cf9345497d5baa79e47721
Daeul Lee <daeul.lee12@gmail.com>
2023-09-25 22:18:36 +0900

    Time: 108 ms (32.03%), Space: 21.4 MB (34.8%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,34 +1,18 @@
-class Solution(object):
-    def groupAnagrams(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: List[List[str]]
-        """
-        #solution 1: sorted() function, -> O(m*nlogn)
-        # hmap = {}
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
 
-        # for word in strs:
-        #     s_word = str(sorted(word))
-        #     if s_word in hmap.keys():
-        #         hmap[s_word].append(word)
-        #     else:
-        #         hmap[s_word] = [word]
-
-        # return hmap.values()
-
-        #solution 2: 
-
-        res = defaultdict(list)
+        hmap = defaultdict(list)
 
         for word in strs:
-            count = [0] * 26
-
+            temp = [0] * 26
             for c in word:
-                count[ord(c) - ord('a')] += 1
-          
-            res[tuple(count)].append(word)
-        
-        return res.values()
+                temp[ord(c) - ord('a')] += 1
             
+            hmap[tuple(temp)].append(word)
 
-
+        res = []
+        for key in hmap:
+            res.append(hmap[key])
+        
+        return res
+        
