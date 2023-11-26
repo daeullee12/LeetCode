488ec91d7d5d729e597c7636445682ce678c1f7d
Daeul Lee <daeul.lee12@gmail.com>
2023-11-26 11:32:17 -0600

    Time: 56 ms, Memory: 16.7 MB - LeetHub

--- largest-unique-number/largest-unique-number.py
+++ largest-unique-number/largest-unique-number.py
@@ -1,15 +1,10 @@
 class Solution:
     def largestUniqueNumber(self, nums: List[int]) -> int:
-        once = set()
-        more = set()
+        count = collections.Counter(nums)
+        answer = -1
         
-        for n in nums:
-            if n not in once and n not in more:
-                once.add(n)
-            elif n in once:
-                once.remove(n)
-                more.add(n)
-            else:
-                continue
-            
-        return max(list(once)) if len(once) != 0 else -1
+        for k, v in count.items():
+            if v == 1:
+                answer = max(answer, k)
+        
+        return answer
