e675d127f352d51ea114139b501c55eab6925c01
Daeul Lee <daeul.lee12@gmail.com>
2023-12-26 15:38:16 -0600

    Time: 37 ms, Memory: 17.5 MB - LeetHub

--- maximum-number-of-balloons/maximum-number-of-balloons.py
+++ maximum-number-of-balloons/maximum-number-of-balloons.py
@@ -1,14 +1,9 @@
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
-        b  = text.count('b')
-        a  = text.count('a')
-        l  = text.count('l')//2
-        o  = text.count('o')//2
-        n  = text.count('n')
+        b = text.count('b')
+        a = text.count('a')
+        l = text.count('l') // 2
+        o = text.count('o') // 2
+        n = text.count('n')
         
-        return min(b,a,l,o,n)
-            
-                
-                
-                
-                
+        return min(b, a, l, o, n)
