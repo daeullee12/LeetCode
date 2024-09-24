94df211eb36b2a9ada12a23bb1837799d8160939
Daeul Lee <dlee50@cougarnet.uh.edu>
2024-09-24 08:47:21 -0500

    Time: 76 ms (24.36%), Space: 17 MB (55.44%) - LeetHub

--- 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
@@ -17,7 +17,7 @@ class Codec:
             j = i
             while s[j] != "#":
                 j += 1
-            length = int(s[i])
+            length = int(s[i:j])
             res.append(s[j + 1:j + 1 + length])
             i = j + 1 + length
 
